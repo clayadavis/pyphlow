23109c31ea2ce2f5aad6affadf89f05ea52daef0
Clayton Davis <claydavi@indiana.edu>
2013-01-25 19:33:53 -0800

    Undirected simplification works.

--- pyphlow.py
+++ pyphlow.py
@@ -1,33 +1,41 @@
-import networkx as nx
-from networkx.algorithms import bipartite
-try:
-    import simplejson as json
-except ImportError:
-    import json
-# import matplotlib.pyplot as plt
 """
-G = Graph()
+G = pyphlow.Graph()
 G.add_edges_from([(1,2),(2,6)])
 G.add_cycle([2,3,4,5])
 G.node[1]['source']=True
 G.node[6]['sink']=True
 G.nodes(True)
+S = pyphlow.simplify(G)
+S
 
 """
+import networkx as nx
+from networkx.algorithms import bipartite
+import itertools
+try:
+    import simplejson as json
+except ImportError:
+    import json
+    
+# import matplotlib.pyplot as plt
+
 
 class Graph(nx.Graph):
+    """This is a trivial extension of networkx.Graph which adds a 
+    better __repr__."""
     def __repr__(self):
         return "G"+str(self.nodes())
 
 ### Undirected
+### TODO: Add weights.
 def block_cutpoint_tree(G, projected=False, verbose=False):
-    in_graph = Graph(G)
+    input_graph = Graph(G)
     top_nodes = []
     bottom_nodes = []
-    articulation_points = set(nx.articulation_points(in_graph))
+    articulation_points = set(nx.articulation_points(input_graph))
     if verbose:
         print "Articulation points:", articulation_points
-    for biconnected_component in nx.biconnected_components(in_graph):
+    for biconnected_component in nx.biconnected_components(input_graph):
         inter = biconnected_component.intersection(articulation_points)
         if verbose:
             print "Inter:", inter
@@ -43,19 +51,82 @@ def block_cutpoint_tree(G, projected=False, verbose=False):
     edges = zip(top_nodes, bottom_nodes)
     if verbose:
         print "Edges:", edges
-    bct = Graph()
-    bct.add_edges_from(edges)
+    bc_tree = Graph()
+    bc_tree.add_edges_from(edges)
     if projected:
-        return Graph(bipartite.projected_graph(bct, top_nodes))
+        return Graph(bipartite.projected_graph(bc_tree, top_nodes))
     else:
-        return bct
+        return bc_tree
+        
+def find_source_and_sink(G, as_dict=False):
+    sources = []
+    sinks = []
+    for node, attrs in G.nodes(True):
+        if attrs.get('source', False):
+            sources.append(node)
+        if attrs.get('sink', False):
+            sinks.append(node)
+    if as_dict:
+        return {'source':sources, 'sink':sinks}
+    else:
+        return (sources, sinks)
+        
+def get_matching_nodes(pattern, G, with_json=True):
+    matches = []
+    for node in G:
+        if with_json:
+            seq = json.loads(node)
+        else:
+            seq = node
+        if pattern in seq:
+            matches.append(node)
+    return matches
+    
+def simplify(G, with_apsp=True, verbose=False):
+    bc_tree = block_cutpoint_tree(G, projected=True)
+    (sources, sinks) = find_source_and_sink(G)
+    apsp = None
+    if with_apsp:
+        apsp = nx.shortest_path(bc_tree)
+    essential_paths = []
+    for (source, sink) in itertools.product(sources, sinks):
+        if verbose:
+            print "Looking for shortest path from %s to %s..." % (source, sink)
+        source_bccs = get_matching_nodes(source, bc_tree)
+        sink_bccs = get_matching_nodes(sink, bc_tree)
+        shortest_path = None
+        for (source_bcc, sink_bcc) in itertools.product(source_bccs, sink_bccs):
+            if with_apsp:
+                possible_path = apsp[source_bcc][sink_bcc]
+            else:
+                possible_path = nx.shortest_path(bc_tree, source_bcc, sink_bcc)
+            if verbose:
+                print "... possible path from %s to %s has length %i: %s" % \
+                    (source_bcc, sink_bcc, len(possible_path), possible_path)
+            # This might be less than ideal, in case there are more than one
+            if not shortest_path or len(possible_path) < len(shortest_path):
+                shortest_path = possible_path
+        essential_paths.append(shortest_path)
+        if verbose:
+            print "--> shortest path from %s to %s: %s" % \
+                (source, sink, shortest_path)
+        
+    essential_nodes = set()
+    for e_path in essential_paths:
+        for e_node in e_path:
+            for node in json.loads(e_node):
+                essential_nodes.add(node)
+    if verbose:
+        print "Essential nodes:", sorted(essential_nodes)
+    return G.subgraph(essential_nodes)
+            
 
-#bct = block_cutpoint_tree(G)
 #Find the shortest path on block_cutpoint_tree from s to t
 #Prune everything not on that path, then project graph
 
 ### Directed
 
+### Work-in-progress
 def triangle_squash(G):
     done = False
     while not done:
